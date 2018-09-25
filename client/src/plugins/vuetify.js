82eaa9a950cff69341b5f75e6929be241c2c0dd2
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-09-25 16:47:52 -0500

    Adding NavBar

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,7 +1,15 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
-})
+  theme: {
+    primary: "#FBC02D",
+    secondary: "#F44708",
+    accent: "#E65100",
+    error: "#D10000",
+    warning: "#B71C1C",
+    info: "#1A237E",
+    success: "#04E762"
+  }
+});
