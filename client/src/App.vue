3234ec0c2d5379cd8e1e46cc3a42df2b2a3e6094
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-09-25 17:30:43 -0500

    Adding Sidebar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,32 @@
 <template>
-  <v-app>
+  <v-app style="background: #FFF176">
+    <!-- Side Navbar -->
+    <v-navigation-drawer app temporary fixed v-model="sideNav">
+      <v-toolbar color="accent" dark flat>
+        <v-toolbar-side-icon @click="toggleSideNav"></v-toolbar-side-icon>
+        <router-link to="/" tag="span" style="cursor:pointer">
+          <h1 class="title pl-3">Foodies</h1>
+        </router-link>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+      <!-- Side Navbar Links -->
+      <v-list>
+        <v-list-tile ripple v-for="item in sideNavItems" :key="item.title" :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            {{item.title}}
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <!-- Horizontal NavBar -->
 
     <v-toolbar fixed color="primary" dark>
       <!-- App Title -->
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="toggleSideNav"></v-toolbar-side-icon>
       <v-toolbar-title class="hidden-xs-only">
         <router-link to="/" tag="span" style="cursor:pointer">
           Foodies
@@ -13,13 +35,13 @@
       <v-spacer></v-spacer>
 
       <!-- Search Input -->
-      <v-text-field flex prepend-icon="search" placeholder="Search" color="accent" single-line hide-details></v-text-field>
+      <v-text-field flex prepend-icon="search" placeholder="Buscar post" color="accent" single-line hide-details></v-text-field>
       <v-spacer></v-spacer>
 
       <!-- Horizontal NavBar Links -->
-      <v-toolbar-items>
+      <v-toolbar-items class="hidden-xs-only">
         <v-btn flat v-for="item in horizontalNavItems" :key="item.title" to="item.link">
-          <v-icon class="hidden-md-only" left>{{item.icon}}</v-icon>
+          <v-icon class="hidden-sm-only" left>{{item.icon}}</v-icon>
           {{item.title}}
         </v-btn>
       </v-toolbar-items>
@@ -27,8 +49,9 @@
 
     <!-- App Content -->
     <main>
-
-      <router-view />
+      <v-container class="mt-4">
+        <router-view />
+      </v-container>
     </main>
   </v-app>
 </template>
@@ -37,6 +60,11 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      sideNav: false
+    };
+  },
   computed: {
     horizontalNavItems() {
       return [
@@ -44,6 +72,18 @@ export default {
         { icon: "lock_open", title: "Sign In", link: "/signin" },
         { icon: "create", title: "Sign Un", link: "/signun" }
       ];
+    },
+    sideNavItems() {
+      return [
+        { icon: "chat", title: "Post", link: "/posts" },
+        { icon: "lock_open", title: "Sign In", link: "/signin" },
+        { icon: "create", title: "Sign Up", link: "/signup" }
+      ];
+    }
+  },
+  methods: {
+    toggleSideNav() {
+      this.sideNav = !this.sideNav;
     }
   }
 };
