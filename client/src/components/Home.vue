c31c4b231f8103ac00bbe6be54f50f9c8f78b5ba
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-09-27 09:44:17 -0500

    Adding SignIn User function

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,42 +1,58 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
-    <ApolloQuery :query="getPostQuery">
-      <template slot-scope="{ result: { loading, error, data }}">
-        <div v-if="loading">Loading...</div>
-        <div v-else-if="error">Error! {{error.message}}</div>
-        <div v-else-if="!loading">Network Status {{networkStatus}}</div>
-        <ul v-else v-for="post in data.getPosts" :key="post._id">
-          <li>
-            {{post.title}}
-            {{post.imageUrl}}
-            {{post.likes}}
-          </li>
-        </ul>
-      </template>
-    </ApolloQuery>
+  <v-container text-xs-center>
+    <v-layout row>
+      <v-dialog v-model="loading" persistent fullscreen>
+        <v-container fill-height>
+          <v-layout row justify-center align-center>
+            <v-progress-circular indetermite :size='70' :width='7' color="secondary"></v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
+    </v-layout>
+    <v-flex xs12>
+      <v-carousel v-if="!loading && posts.length > 0" v-bind="{'cycle': true}" interval="3000">
+        <v-carousel-item v-for="post in posts" :key="post._id" :src="post.imageUrl">
+          <h1 id="carousel__title">{{post.title}}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
   </v-container>
 </template>
 
 <script>
-import { gql } from "apollo-boost";
+
+import { mapGetters } from 'vuex';
 
 export default {
   name: "home",
-  data() {
-    return {
-      getPostQuery: gql`
-        query {
-          getPosts {
-            _id
-            title
-            imageUrl
-            description
-            likes
-          }
-        }
-      `
-    };
+  created(){
+    this.handleGetCarouselPosts();  
+  },
+  computed: {
+    ...mapGetters(['loading', 'posts'])
+  },
+  methods: {
+    handleGetCarouselPosts(){
+      this.$store.dispatch('getPosts');
+    }
   }
 };
+
+
 </script>
+
+
+<style>
+
+#carousel__title{
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5pc 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0; 
+}
+</style>
