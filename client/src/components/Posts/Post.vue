01dc9f4cf3f23728295c2c185e0b6fc2da051bd4
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-10-03 07:11:55 -0500

    Adding comments

--- client/src/components/Posts/Post.vue
+++ client/src/components/Posts/Post.vue
@@ -38,98 +38,125 @@
       </v-layout>
 
 
+    <!-- Messages Section -->
+    <div class="mt-3">
+      <!-- Message Input -->
+      <v-layout class="mb-3" v-if="user">
+        <v-flex xs12>
+          <v-form @submit.prevent="handleAddPostMessage">
+            <v-layout row>
+              <v-flex xs12>
+                <v-text-field v-model="messageBody" clearable :append-outer-icon="messageBody && 'send'" label="Agrega un Comentario" type="text" @click:append-outer="handleAddPostMessage" prepend-icon="email" required></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-form>
+        </v-flex>
+      </v-layout>
+
       <!-- Messages -->
-      <div class="mt-3">
-        <!-- Message Input -->
-        <v-layout class="mb-3" v-if="user">
-          <v-flex xs12>
-            <v-form>
-              <v-layout row>
-                <v-flex xs12>
-                  <v-text-field clearable append-outer-icon="send" label="Agrega un comentario." type="text" prepend-icon="email">
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-list subheader two-line>
+            <v-subheader>Comentarios ({{getPost.messages.length}}) </v-subheader>
 
-                  </v-text-field>
-                </v-flex>
-              </v-layout>
-            </v-form>
-          </v-flex>
-        </v-layout>
+            <template v-for="message in getPost.messages">
+              <v-divider :key="message._id"></v-divider>
 
-        <!-- Messages List -->
-        <v-layout row wrap>
-          <v-flex xs12>
-            <v-list subheader two-line>
-              <v-subheader>Comentarios ({{getPost.messages.length}})</v-subheader>
+              <v-list-tile avatar inset :key="message.title">
+                <v-list-tile-avatar>
+                  <img :src="message.messageUser.avatar">
+                </v-list-tile-avatar>
 
-              <template v-for="message in getPost.messages">
-                <v-divider :key="message._id"></v-divider>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{message.messageBody}}
+                  </v-list-tile-title>
+                  <v-list-tile-sub-title>
+                    {{message.messageUser.username}}
+                    <span class="grey--text text--lighten-1 hidden-xs-only">{{message.messageDate}}</span>
+                  </v-list-tile-sub-title>
+                </v-list-tile-content>
 
+                <v-list-tile-action class='hidden-xs-only'>
+                  <v-icon color="grey">chat_bubble</v-icon>
+                </v-list-tile-action>
 
-                <v-list-tile avatar inset :key="message.title">
-                  <v-list-tile-avatar>
-                    <img :src="message.messageUser.avatar">
-                  </v-list-tile-avatar>
+              </v-list-tile>
+            </template>
+          </v-list>
+        </v-flex>
+      </v-layout>
 
-                  <v-list-tile-content>
-                    <v-list-tile-title>
-                      {{message.messageBody}}
-                    </v-list-tile-title>
-
-                    <v-list-tile-sub-title>
-                      {{message.messageUser.username}}
-                      <span class="grey--text text-lighten-1 hidden-xs-only">{{message.messageDate}}</span>
-                    </v-list-tile-sub-title>
-                  </v-list-tile-content>
-
-                  <v-list-tile-action class="hidden-xs-only" >
-                    <v-icon color="grey">chat_bubble</v-icon>
-                  </v-list-tile-action>
-                </v-list-tile>
-              </template>
-            </v-list>
-          </v-flex>
-        </v-layout>
-      </div>
+    </div>
     </v-container>
 </template>
 
 <script>
 
 import {mapGetters} from 'vuex';
-import {GET_POST} from '../../queries';
+import {GET_POST, ADD_POST_MESSAGE} from '../../queries';
 
 export default {
-    name: 'Post',
-    props: ['postId'],
-    data(){
-      return {
-        dialog: false
+  name: "Post",
+  props: ["postId"],
+  data() {
+    return {
+      dialog: false,
+      messageBody: ""
+    };
+  },
+  apollo: {
+    getPost: {
+      query: GET_POST,
+      variables() {
+        return {
+          postId: this.postId
+        };
       }
+    }
+  },
+  computed: {
+    ...mapGetters(["user"])
+  },
+  methods: {
+    handleAddPostMessage() {
+      const variables = {
+        messageBody: this.messageBody,
+        userId: this.user._id,
+        postId: this.postId
+      };
+      this.$apollo
+        .mutate({
+          mutation: ADD_POST_MESSAGE,
+          variables,
+          update: (cache, { data: { addPostMessage } }) => {
+            const data = cache.readQuery({
+              query: GET_POST,
+              variables: { postId: this.postId }
+            });
+            data.getPost.messages.unshift(addPostMessage);
+            cache.writeQuery({
+              query: GET_POST,
+              variables: { postId: this.postId },
+              data
+            });
+          }
+        })
+        .then(({ data }) => {
+          console.log(data.addPostMessage);
+        })
+        .catch(err => console.error(err));
     },
-    apollo:{
-        getPost: {
-            query: GET_POST,
-            variables(){
-                return {
-                    postId: this.postId
-                }
-            }
-        }
+    goToPreviousPage() {
+      this.$router.go(-1);
     },
-    computed: {
-      ...mapGetters(['user'])
-    },
-    methods: {
-      goToPreviousPage(){
-        this.$router.go(-1);
-      },
-      toggleImageDialog(){
-        if(window.innerWidth > 500){
-          this.dialog = !this.dialog;
-        }
+    toggleImageDialog() {
+      if (window.innerWidth > 500) {
+        this.dialog = !this.dialog;
       }
-    }   
-}
+    }
+  }
+};
 </script>
 
 <style scoped>
